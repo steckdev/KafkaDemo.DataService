5b3ef6a6a55f5f295b50f75b2dd2bf388e77de18
Tyler Steck <tyler.steck@dexi.io>
2021-11-18 07:15:13 -0700

    Dev-1: Initial commit of Customer Data Processor

---
+++ DataService/CustomerDataProcessor/Controllers/CustomerPurchasesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerPurchasesController : ControllerBase
+    {
+
+        private readonly ILogger<CustomerPurchasesController> _logger;
+
+        public CustomerPurchasesController(ILogger<CustomerPurchasesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<DataService.CustomerPurchasesDto> Get()
+        {
+            var rng = new Random();
+            return new List<CustomerPurchasesDto>{
+                new CustomerPurchasesDto{ Id = 1 }
+                };
+            }
+    }
+}
---
+++ DataService/CustomerDataProcessor/Models/Dto/CustomerPurchasesDto.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace DataService
+{
+    public class CustomerPurchasesDto
+    {
+        public int Id { get; set; }
+    }
+}
