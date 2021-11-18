d9a4870a687e4f06970749b23c9e3b19f59e4e58
Tyler Steck <tyler.steck@dexi.io>
2021-11-18 08:28:03 -0700

    DEV-1: Working on DTO and service validation

--- DataService/CustomerDataProcessor/Controllers/CustomerPurchasesController.cs
+++ DataService/CustomerDataProcessor/Controllers/CustomerPurchasesController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace DataService.Controllers
+namespace DataService.Controllers.v1
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]", Order = 1)]
+    [Route("api/v{version:apiVersion}/[controller]", Order = 1)]
+    [ApiVersion("1.0")]
     public class CustomerPurchasesController : ControllerBase
     {
 
@@ -20,12 +22,48 @@ namespace DataService.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<DataService.CustomerPurchasesDto> Get()
+        public IActionResult Status()
         {
+            //Check dependent services
+            return Ok(new ServiceStatusModel
+            {
+                ServiceName = this.GetType().Name,
+                StatusCode = 200,
+                Status = ServiceStatusModel.StatusCodeEnum.Ok,
+                ExternalServices = new[] {
+                    new ServiceStatusModel {
+                        ServiceName = "KafkaService",
+                        Status = ServiceStatusModel.StatusCodeEnum.Ok,
+                        StatusCode = 200
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// Receives list of purchases and then processes those items into Kafka for futher processing. The items returned are 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("CustomerData/orders")]
+        public IEnumerable<DataService.CustomerPurchasesDto> CustomerData(IEnumerable<DataService.CustomerPurchasesDto> data)
+        {
+            var failedItems = data.Any(x => ValidateCustomerPurchaseData(x) == false);
+            ValidateCustomerPurchaseData(data);
             var rng = new Random();
-            return new List<CustomerPurchasesDto>{
-                new CustomerPurchasesDto{ Id = 1 }
+            if (true)
+                return new List<CustomerPurchasesDto>{
+                    new CustomerPurchasesDto{ Id = 1 }
                 };
-            }
+            else
+                return Enumerable.Empty<DataService.CustomerPurchasesDto>();
+        }
+
+         void ValidateCustomerPurchaseData(CustomerPurchasesDto data)
+        {
+            if(data.Id < 0 || data.Email.Contains("@")
+            throw new NotImplementedException();
+        }
     }
 }
--- DataService/CustomerDataProcessor/Models/Dto/CustomerPurchasesDto.cs
+++ DataService/CustomerDataProcessor/Models/Dto/CustomerPurchasesDto.cs
@@ -1,9 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataService
 {
     public class CustomerPurchasesDto
     {
+        [Range(0, int.MaxValue)]
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public EmailAddressAttribute Email { get; set; }
+        public AppPurchaseDto Apps { get; set; }
     }
 }
